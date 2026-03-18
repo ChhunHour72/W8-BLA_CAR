08e50d8a9cd00241f5769fbf6cea1b910f7bea79
Chhun Hour <longchhunhour74@gmail.com>
2026-03-18 21:24:58 +0700

    BLA-201 – Repositories

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,41 @@
 
+import 'package:blabla/data/repositories/location/location_repository.dart';
+import 'package:blabla/data/repositories/ride/ride_repository.dart';
+import 'package:blabla/data/repositories/ride_preference/ride_preference_repository.dart';
+import 'package:blabla/model/ride_pref/ride_pref.dart';
 import 'package:flutter/material.dart';
 import 'ui/screens/home/home_screen.dart';
 import 'ui/theme/theme.dart';
 
+import 'data/repositories/location/location_repository_mock.dart';
+import 'data/repositories/ride_preference/ride_preference_repository_mock.dart';
+import 'data/repositories/ride/ride_repository_mock.dart';
+
 void main() {
-   runApp(const BlaBlaApp()); 
+
+  final locationRepository = LocationRepositorysMock();
+  final ridePreferenceRepository = RidePreferenceRepositoryMock();
+  final rideRepository = RideRepositoryMock();
+
+   runApp(BlaBlaApp(
+    locationRepository : locationRepository,
+    ridePreferenceRepository : ridePreferenceRepository,
+    rideRepository : rideRepository,
+   )); 
 }
 
 
 class BlaBlaApp extends StatelessWidget {
-  const BlaBlaApp({super.key});
+
+  final LocationRepositorysMock locationRepository;
+  final RidePreferenceRepositoryMock ridePreferenceRepository;
+  final RideRepository rideRepository;
+  const BlaBlaApp({
+    super.key,
+    required this.locationRepository,
+    required this.ridePreferenceRepository,
+    required this.rideRepository
+    });
 
   @override
   Widget build(BuildContext context) {
